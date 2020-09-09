7d28989b61c8a4765599ad71ca305b47048c8512
Pablo Asensio Martínez <44872361+pasensio97@users.noreply.github.com>
2020-09-09 03:25:53 +0200

    Add files via upload

--- checkers/board.py
+++ checkers/board.py
@@ -34,27 +34,46 @@ class Board:
         board = []
         # first team
         for row in range(3):
-            new_row = [0] * COLS
+            new_row = [None] * COLS
             for col in range((row + 1) % 2, ROWS, 2):
                 new_row[col] = Piece(row, col, WHITE)
             board.append(new_row)
 
         # empty space
         for row in range(3, ROWS - 3):
-            board.append([0] * COLS)
+            board.append([None] * COLS)
 
         # second team
         for row in range(ROWS - 3, ROWS):
-            new_row = [0] * COLS
+            new_row = [None] * COLS
             for col in range((row + 1) % 2, ROWS, 2):
                 new_row[col] = Piece(row, col, COOL_RED)
             board.append(new_row)
         return board
 
+    def draw_possible_movs(self, win):
+        if self.selected_piece is not None:
+            pos_movs = self.selected_piece.possible_movs(self.board)
+            if pos_movs:
+                for dot in pos_movs:
+                    dot.draw(win)
+
     def draw_pieces(self, win):
         for row in range(ROWS):
             for col in range(COLS):
                 piece = self.board[row][col]
-                if piece != 0:
+                if piece != None:
                     piece.draw(win)
+                    if piece == self.selected_piece:
+                        piece.draw_selected(win)
+                        piece.draw_possible_movs(self.board, win)
+
+    def select_piece(self, row, col):
+        self.selected_piece = self.board[row][col]
+        if self.selected_piece is not None:
+            print(self.selected_piece.row, self.selected_piece.col)
+            pos_movs = self.selected_piece.possible_movs(self.board)
+        else:
+            pos_movs = None
+        return self.selected_piece, pos_movs
 
--- checkers/piece.py
+++ checkers/piece.py
@@ -1,6 +1,6 @@
 import os
 import pygame
-from .constants import COOL_RED, SQUARE_SIZE, BLACK, COOL_GOLD
+from .constants import COOL_RED, SQUARE_SIZE, BLACK, COOL_GOLD, ROWS, COLS, GREEN
 
 
 class Piece:
@@ -12,7 +12,7 @@ class Piece:
         self.row = row
         self.col = col
         self.color = color
-        self.king = False
+        self.king = True
 
         try:
             self.img = pygame.image.load(self.CROWN_FILE)
@@ -40,19 +40,79 @@ class Piece:
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
         self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
 
-    def make_king(self):
-        self.king = True
+    def draw_king(self, win):
+        rect = self.img.get_rect()
+        rect = rect.move(self.x - SQUARE_SIZE // 4, self.y - SQUARE_SIZE // 4)
+        win.blit(self.img, rect)
 
     def draw(self, win):
         radius = SQUARE_SIZE // 2 - self.PADDING
         # pygame.draw.circle(win, BLACK, (self.x, self.y), radius + self.OUTLINE)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius)
         if self.king:
-            rect = self.img.get_rect()
-            rect = rect.move(self.x - SQUARE_SIZE // 4, self.y - SQUARE_SIZE // 4)
-            win.blit(self.img, rect)
-            # pygame.draw.circle(win, COOL_GOLD, (self.x, self.y), radius // 2)
+            self.draw_king(win)
+
+    def possible_movs(self, board):
+        pos_movs = []
+        if not self.king:
+            # Up team
+            if self.direction == 1:
+                potential_movs = [
+                    (self.row + 1, self.col + 1),
+                    (self.row + 1, self.col - 1),
+                ]
+            # Down team
+            else:
+                potential_movs = [
+                    (self.row - 1, self.col + 1),
+                    (self.row - 1, self.col - 1),
+                ]
+
+            for mov in potential_movs:
+                if mov[0] >= 0 and mov[1] >= 0 and mov[0] < ROWS and mov[1] < COLS:
+                    if board[mov[0]][mov[1]] is None:
+                        pos_movs.append(Dot(mov[0], mov[1]))
+
+        else:
+            pass  # print("I'm a king")
+
+        return pos_movs
+
+    def draw_selected(self, win):
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        pygame.draw.circle(win, COOL_GOLD, (self.x, self.y), radius - radius // 8)
+        if self.king:
+            self.draw_king(win)
+
+    def draw_possible_movs(self, board, win):
+        pos_movs = self.possible_movs(board)
+        if pos_movs:
+            for dot in pos_movs:
+                dot.draw(win)
 
     def __repr__(self):
         return str(self.color)
 
+
+class Dot:
+    PADDING = 10
+
+    def __init__(self, row, col, color=GREEN):
+        self.row = row
+        self.col = col
+        self.color = color
+
+        self.x = 0
+        self.y = 0
+        self.calc_pos()
+
+    def calc_pos(self):
+        self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
+        self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
+
+    def draw(self, win):
+        radius = SQUARE_SIZE // 2 - self.PADDING
+        pygame.draw.circle(win, self.color, (self.x, self.y), radius // 2)
+
+    def __repr__(self):
+        return "Dot: [" + str(self.row) + " " + str(self.col) + "]"
